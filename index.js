968618c8203009c2dbc3b6d7aaae99ca199ded4b
Shivangam Soni <shivangamsoni99@gmail.com>
2025-01-27 18:06:53 +0530

    Connect to DB

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const apiRouter = require("./api");
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api", apiRouter);
 
-app.listen(3000, () => console.log("Running on PORT 3000"));
+app.listen(3000, () => {
+    mongoose
+        .connect(process.env.DB_URL)
+        .then(() => console.log("Connected to Database"))
+        .catch(() => console.log("Error Connecting to DB"));
+
+    console.log("Running on PORT 3000");
+});
